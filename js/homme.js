f89e18f1f5aed6573ed83656c1f052fabfa463f8
Brian Okarski <studio@brianokarski.com>
2014-04-16 23:33:11 -0400

    CSS animations

--- js/homme.js
+++ js/homme.js
@@ -3,41 +3,33 @@ $(document).ready(function() {
 
 });
 
-function makeNewPosition() {
+// function makeNewPosition() {
 
-    var h = $(window).height();
-    var w = $(window).width();
+//     var h = $(window).height();
+//     var w = $(window).width();
 
-    var nh = Math.floor(Math.random() * h);
-    var nw = Math.floor(Math.random() * w);
+//     var nh = Math.floor(Math.random() * h);
+//     var nw = Math.floor(Math.random() * w);
 
-    return [nh, nw];
+//     return [nh, nw];
 
-}
+// }
 
-function animateDiv() {
-    var newq = makeNewPosition();
-    $('.img-1').animate({
-        top: newq[0],
-        left: newq[1]
-    }, 3500, function() {
-        animateDiv();
-    });
+// function animateDiv() {
+//     var newq = makeNewPosition();
+//     $('.img-2').animate({
+//         top: newq[0],
+//         left: newq[1]
+//     }, 3000, function() {
+//         animateDiv();
+//     });
 
-    var newq = makeNewPosition();
-    $('.img-2').animate({
-        top: newq[0],
-        left: newq[1]
-    }, 3000, function() {
-        animateDiv();
-    });
+//     var newq = makeNewPosition();
+//     $('.img-3').animate({
+//         top: newq[0],
+//         left: newq[1]
+//     }, 2000, function() {
+//         animateDiv();
+//     });
 
-    var newq = makeNewPosition();
-    $('.img-3').animate({
-        top: newq[0],
-        left: newq[1]
-    }, 2000, function() {
-        animateDiv();
-    });
-
-};
+// };
